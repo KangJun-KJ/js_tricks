e6832440367de1f7aacc57612000aa0d42e55c58
KangJun-KJ <892498546@qq.com>
2019-06-26 13:32:36 +0800

    no message

---
+++ docs/.vuepress/components/comment/comment.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'comment',
+  data() {
+    return {};
+  },
+  mounted() {
+    let body = document.querySelector('.gitalk-container');
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: '20afc81e0f2531720d99',
+        clientSecret: '2e6c7455359ec0f670461768a081bcba0638e570',
+        repo: 'js_tricks',
+        owner: 'KangJun-KJ',
+        // 这里接受一个数组，可以添加多个管理员
+        admin: ['KangJun-KJ'],
+        // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
+       
+        // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
+        id: location.pathname,
+        distractionFreeMode: false,
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render('gitalk-container');
+    };
+  },
+};
+</script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,36 @@
 module.exports = {
-    title: 'JS_ticks',
-    description: 'Just playing around',
-    base: "/js_tricks/",
-    description: '常用的js方法，js_tricks',
-    dest: './dist',
-    repo: "https://github.com/KangJun-KJ/js_tricks/tree/gh-pages",
-    head: [
-        ['link', { rel: 'icon', href: `/images/favicon.png` }],
-        ['meta', { name: 'theme-color', content: '#00adb5' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#00adb5' }],
-        ['meta', { name: 'description', itemprop: 'description', content: '常用的js方法，js_tricks' }],
-        ['meta', { itemprop: 'name', content: 'js_trick' }],
-        ['meta', { itemprop: 'image', content: '/js_tricks/images/favicon.png' }],
-    ],
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'github', link: 'https://github.com/KangJun-KJ/js_tricks' },
-        ],
-        sidebar: [
-            '/bom/',
-            '/dom/',
-            '/array/',
-            '/date/'
-        ]
-    },
-    markdown: {
-        anchor: { permalink: false },
-        toc: { includeLevel: [1, 2] },
-        config: md => {
-            md.use(require('markdown-it-include'), './')
-        }
-    },
-    configureWebpack: {
-        resolve: {
-            alias: {
-            }
-        }
-    }
+  title: 'JS_ticks',
+  description: 'Just playing around',
+  base: "/js_tricks/",
+  description: '常用的js方法，js_tricks',
+  dest: './dist',
+  repo: "https://github.com/KangJun-KJ/js_tricks/",
+  head: [
+      ['link', { rel: 'icon', href: `/images/favicon.png` }]
+  ],
+  themeConfig: {
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'github', link: 'https://github.com/KangJun-KJ/js_tricks' },
+      ],
+      sidebar: [
+          '/bom/',
+          '/dom/',
+          '/array/',
+          '/date/'
+      ]
+  },
+  markdown: {
+      anchor: { permalink: false },
+      toc: { includeLevel: [1, 2] },
+      config: md => {
+          md.use(require('markdown-it-include'), './')
+      }
+  },
+  configureWebpack: {
+      resolve: {
+          alias: {
+          }
+      }
+  }
 }
